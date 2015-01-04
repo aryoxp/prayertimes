1a60380a91f5e2236e67117766e01764b5850cd2
Aryo Pinandito <aryoxp@gmail.com>
2015-01-04 18:44:21 +0700

    fixed bug for unknown location where locality information is unavailable but feature name exists.

--- src/ap/mobile/prayertimes/utilities/LocationHelper.java
+++ src/ap/mobile/prayertimes/utilities/LocationHelper.java
@@ -13,8 +13,11 @@ public class LocationHelper {
 			Geocoder gcd = new Geocoder(context, Locale.getDefault());
 			List<Address> addresses;
 			addresses = gcd.getFromLocation(lat, lng, 1);
-			if (addresses.size() > 0) 
-			    return addresses.get(0).getLocality();
+			if (addresses.size() > 0) {
+				if(addresses.get(0).getLocality() != null) 
+					return addresses.get(0).getLocality();
+				return addresses.get(0).getFeatureName();
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
